fa976b923a86007e545f0c007cf15be1b6b48a58
Sergei Gridnev <sergeigr32@gmail.com>
2022-11-22 02:06:25 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        generateClients();
+        Queue<Person>queue = new LinkedList<>(generateClients());
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 
     private static List<Person> generateClients(){
+        ArrayList<Person> visitors = new ArrayList<>();
         Person person = new Person("Vasya", "Petrov", 3);
         Person person1 = new Person("Petya", "Gagarin", 5);
         Person person2 = new Person("Olya", "Koroleva", 1);
@@ -13,5 +21,13 @@ public class Main {
         Person person4 = new Person("Kolya", "Nikolayev", 31);
         Person person5 = new Person("Tanya", "Linchburger", 7);
 
+        visitors.add(person);
+        visitors.add(person1);
+        visitors.add(person2);
+        visitors.add(person3);
+        visitors.add(person4);
+        visitors.add(person5);
+
+        return visitors;
     }
 }
